17484c9d08dd30bd55838ef618a83c6221a821e9
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2021-12-02 20:13:55 +0700

    Day 3

---
+++ Person/src/com/rupp/Main.java
@@ -0,0 +1,13 @@
+package com.rupp;
+
+public class Main {
+
+    class DemoPer{
+        String name;
+        String sex;
+        int age;
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
